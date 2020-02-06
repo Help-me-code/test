4a3797b7e27f0ed53feea35c0b14f752ede13b24
Help-me-code <60729288+Help-me-code@users.noreply.github.com>
2020-02-06 15:11:46 +0800

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {COURSES} from '../db-data';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+
+
+
 }
