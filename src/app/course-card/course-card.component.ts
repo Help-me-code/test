dcbbec657d0747857f7d383a7751cb064c7daa0d
Help-me-code <dominic.columna@bcstechnology.com.au>
2020-02-06 15:58:35 +0800

    4 files modified, 3 added

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+
+@Input()
+  title:string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
